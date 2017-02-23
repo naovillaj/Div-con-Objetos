5cfe579cc28533b69ccea468c9752f6b88c73b50
Naomi Villanueva <naovillaj@gmail.com>
2017-02-23 14:53:40 -0500

    Div con objetos

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+var records= document.getElementById('records');
+var a = "";
+
+var trabajadorLaboratoria = [ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" }, 
+							  { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" }, 
+							  { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" }, 
+							  { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" }, 
+							  { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
+
+
+ trabajadorLaboratoria.forEach(function(e){ a += "<ul><li>" + "Nombre: " + e.nombre + "</li>" +
+														 "<li>"	+ "Apellido: " + e.apellido + "</li>" +
+														 "<li>" + "Rol: " + e.rol + "</li>" +
+														 "<li>" + "Cumpleaños: " + e.cumpleanios + "</li></ul>";
+
+														records.innerHTML = a; 
+
+														});
+
+ 
+
